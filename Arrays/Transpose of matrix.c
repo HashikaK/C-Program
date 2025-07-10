f22714d33bed7dc41a60eb6c355ab8a95b1de691
Hashika K <hashikakesavan@gmail.com>
2025-07-10 22:24:21 +0530

    Arrays

---
+++ Arrays/Transpose of matrix.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    int matrix[10][10], transpose[10][10];
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+    printf("Enter matrix elements:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+    printf("Transpose of the matrix:\n");
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < r; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Arrays/Two matrix are equal or not.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10];
+    int i, j, r, c;
+    int equal = 1;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            if (a[i][j] != b[i][j]) {
+                equal = 0;
+                break;
+            }
+        }
+    }
+    if (equal == 1)
+        printf("Matrices are equal.\n");
+    else
+        printf("Matrices are not equal.\n");
+
+    return 0;
+}
