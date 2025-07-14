dc4e019adb07a8fa2fd16882489a1f4b97d185bb
Hashika K <hashikakesavan@gmail.com>
2025-07-14 14:06:23 +0530

    updated

--- 2D Array/2d array.c
+++ 2D Array/2d array.c
@@ -4,14 +4,12 @@ int main() {
     int arr[10][10];
     int rows, cols, i, j;
     scanf("%d\n%d", &rows, &cols);
-
     printf("Enter elements of the array:\n");
     for (i = 0; i < rows; i++) {
         for (j = 0; j < cols; j++) {
             scanf("%d", &arr[i][j]);
         }
     }
-
     printf("The matrix is:\n");
     for (i = 0; i < rows; i++) {
         for (j = 0; j < cols; j++) {
