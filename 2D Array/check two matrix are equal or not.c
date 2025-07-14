5f1615d2b67cd213b2eeab2778d493c3b28c3afc
Hashika K <hashikakesavan@gmail.com>
2025-07-14 14:07:02 +0530

    matrix

--- 2D Array/check two matrix are equal or not.c
+++ 2D Array/check two matrix are equal or not.c
@@ -4,11 +4,9 @@ int main() {
     int a[10][10], b[10][10];
     int i, j, r, c;
     int equal = 1;
-
     // Input rows and columns
     printf("Enter number of rows and columns: ");
     scanf("%d %d", &r, &c);
-
     // Input first matrix
     printf("Enter elements of first matrix:\n");
     for (i = 0; i < r; i++) {
@@ -16,7 +14,6 @@ int main() {
             scanf("%d", &a[i][j]);
         }
     }
-
     // Input second matrix
     printf("Enter elements of second matrix:\n");
     for (i = 0; i < r; i++) {
@@ -24,7 +21,6 @@ int main() {
             scanf("%d", &b[i][j]);
         }
     }
-
     // Compare both matrices
     for (i = 0; i < r; i++) {
         for (j = 0; j < c; j++) {
@@ -34,12 +30,10 @@ int main() {
             }
         }
     }
-
     // Output result
     if (equal == 1)
         printf("Matrices are equal.\n");
     else
         printf("Matrices are not equal.\n");
-
     return 0;
 }
