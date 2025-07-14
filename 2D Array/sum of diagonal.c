4bac55cd74eb75cc54c55c954de13379c454d4ca
Hashika K <hashikakesavan@gmail.com>
2025-07-14 14:08:28 +0530

    diagonal

--- 2D Array/sum of diagonal.c
+++ 2D Array/sum of diagonal.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 int main() {
     int n, matrix[10][10], sum = 0;
     printf("Enter size of square matrix (n x n): ");
     scanf("%d", &n);
-
     printf("Enter matrix elements:\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
