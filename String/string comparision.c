85d36dbebcc78061f6ac3042fd0ac661b561b35e
Hashika K <hashikakesavan@gmail.com>
2025-08-09 12:03:02 +0530

    string comparision added

---
+++ String/string comparision.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s1[100], s2[100];
+    scanf("%s", s1);
+    scanf("%s", s2);
+    int a = strcmp(s1 + strlen(s1) - 5, ".java") == 0;
+    int b = strcmp(s2 + strlen(s2) - 5, ".java") == 0;
+    if (a && b)
+        printf("2\n");
+    else if (a || b)
+        printf("1\n");
+    else
+        printf("0\n");
+    return 0;
+}
